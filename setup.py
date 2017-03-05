75185dea85c1b6452008761677d69cbb9c3fb358
ISosnovik <sosnovikivan@gmail.com>
2017-03-05 16:10:46 +0300

    change setup

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from pip.req import parse_requirements
 install_reqs = parse_requirements("requirements.txt", session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
+_packages = ['vkapi', 'vkapi/methods']
+
 setup(name='vkapi',
-      version='0.1.8',
+      version='0.1.14',
       description='VK API for Python',
       author='Ivan Sosnovik',
       url='https://github.com/ISosnovik/vkAPI',
-      packages=['.vkapi'],
-      package_dir={'vkapi': ''},
+      packages=_packages,
       install_requires=reqs,
       )
